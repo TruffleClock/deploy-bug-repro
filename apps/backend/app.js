01d188eb93b3ebc1a3bbaa6a1a57ecf8c01270e8
truffleclock <petroleum@gmail.com>
2025-01-27 10:32:31 -0800

    this is a change to apps/backend

--- apps/backend/app.js
+++ apps/backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// this is a change to apps/backend
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
