6313c15e8d62b40366f87f510756565a521622c8
truffleclock <petroleum@gmail.com>
2025-01-27 16:53:17 -0800

    remove watch paths

--- apps/backend/app.js
+++ apps/backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // this is a change to apps/backend
+// hello
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
